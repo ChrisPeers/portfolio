e90e52217807fc1df60dc02046c0e4adca853444
Chris Peers <chrispeers123@hotmail.com>
2021-06-28 15:20:45 +0100

    Typewritter effect on the h2 works, just need to add breakpoints for the cursor to make it the correct size on each screen

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const textH2 = document.getElementById("hero-h2");
+const txt = "I'm a Web Developer";
+const speed = 50;
+let i = 0;
+
+function typeWriter() {
+    if (i < txt.length) {
+        textH2.innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+typeWriter();
+
+
+
+
+
