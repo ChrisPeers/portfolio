53585e2f6b2626e0f17c488d99fdd344340097a9
Chris Peers <chrispeers123@hotmail.com>
2021-06-29 15:19:51 +0100

    created the coding example content and styled it, fixed a few small issues throughout the site

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 const textH2 = document.getElementById("hero-h2");
-const txt = "I'm a Web Developer";
+const text = "I'm a Web Developer";
 const speed = 50;
 let i = 0;
 
 function typeWriter() {
-    if (i < txt.length) {
-        textH2.innerHTML += txt.charAt(i);
+    if (i < text.length) {
+        textH2.innerHTML += text.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
     }
